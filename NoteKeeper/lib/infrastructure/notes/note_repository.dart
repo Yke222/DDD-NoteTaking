f1fa63954fe3d0082740f551615335e6ef0768c0
Luiz Velloso <lvelloso@Luizs-MacBook-Air.local>
2021-02-20 18:39:06 -0300

    NK-16: Watch notes on firestore

---
+++ NoteKeeper/lib/infrastructure/notes/note_repository.dart
@@ -0,0 +1,92 @@
+import 'package:NoteKeeper/domain/notes/i_note_repository.dart';
+import 'package:NoteKeeper/infrastructure/notes/note_dtos.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/services.dart';
+import 'package:injectable/injectable.dart';
+import 'package:kt_dart/kt.dart';
+import 'package:dartz/dartz.dart';
+import 'package:NoteKeeper/domain/notes/note_failure.dart';
+import 'package:NoteKeeper/domain/notes/note.dart';
+import 'package:NoteKeeper/infrastructure/core/firestore_helpers.dart';
+import 'package:rxdart/rxdart.dart';
+
+@LazySingleton(as: INoteRepository)
+class NoteRepository implements INoteRepository {
+  final FirebaseFirestore _firestore;
+
+  NoteRepository(
+    this._firestore,
+  );
+
+  @override
+  Stream<Either<NoteFailure, KtList<Note>>> watchAll() async* {
+    final userDoc = await _firestore.userDocument();
+    yield* userDoc.noteCollection
+        .orderBy(
+          'serverTimeStamp',
+          descending: true,
+        )
+        .snapshots()
+        .map(
+          (snapshot) => right<NoteFailure, KtList<Note>>(
+            snapshot.docs
+                .map((doc) => NoteDTO.fromFirestore(doc).toDomain())
+                .toImmutableList(),
+          ),
+        )
+        .onErrorReturnWith((e) {
+      if (e is PlatformException && e.message.contains('permission-denied')) {
+        return left(const NoteFailure.insufficientPermission());
+      } else {
+        return left(const NoteFailure.unexpected());
+      }
+    });
+  }
+
+  @override
+  Stream<Either<NoteFailure, KtList<Note>>> watchUncompleted() async* {
+    final userDoc = await _firestore.userDocument();
+    yield* userDoc.noteCollection
+        .orderBy(
+          'serverTimeStamp',
+          descending: true,
+        )
+        .snapshots()
+        .map((snapshot) => snapshot.docs.map(
+              (doc) => NoteDTO.fromFirestore(doc).toDomain(),
+            ))
+        .map(
+          (notes) => right<NoteFailure, KtList<Note>>(
+            notes
+                .where((note) =>
+                    note.todos.getOrCrash().any((todoItem) => !todoItem.done))
+                .toImmutableList(),
+          ),
+        )
+        .onErrorReturnWith((e) {
+      if (e is PlatformException && e.message.contains('permission-denied')) {
+        return left(const NoteFailure.insufficientPermission());
+      } else {
+        return left(const NoteFailure.unexpected());
+      }
+    });
+  }
+
+  @override
+  Future<Either<NoteFailure, Unit>> create(Note note) {
+    // TODO: implement create
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<NoteFailure, Unit>> delete(Note note) {
+    // TODO: implement delete
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<NoteFailure, Unit>> update(Note note) {
+    // TODO: implement update
+    throw UnimplementedError();
+  }
+}
